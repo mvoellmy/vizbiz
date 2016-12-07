30665e2e81533e78221666f90ffe65b2263ead4e
duboisf <duboisf@student.ethz.ch>
2016-12-07 14:06:59 +0100

    let's take some calib images ;)

--- src/calibration/main.m
+++ src/calibration/main.m
@@ -2,7 +2,7 @@ clear all;
 close all;
 clc;
 
-[video_name,path_name] = uigetfile('*.*','Select the video source file');
+[video_name,path_name] = uigetfile('*.*','Select the video source file...');
 
 options.save_images = true;
 options.extract_every_x = 10; % for all images set: 1
