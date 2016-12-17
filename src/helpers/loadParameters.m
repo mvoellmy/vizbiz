579741b3a638478f40c0c5430cbe2d9048520078
duboisf <duboisf@student.ethz.ch>
2016-12-17 13:39:27 +0100

    tested

--- src/helpers/loadParameters.m
+++ src/helpers/loadParameters.m
@@ -8,7 +8,7 @@ function params = loadParameters(mode)
 %  - params(struct) : parameter struct
 
 % general parameters
-params.ds = 2; % 0: KITTI, 1: Malaga, 2: Parking
+params.ds = 1; % 0: KITTI, 1: Malaga, 2: Parking
 params.auto_bootstrap = true;
 params.perf.profiling = false;
 params.compare_against_groundthruth = false;
