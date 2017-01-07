2a313ea8db93d09b29a9790dd91658e5622caf97
duboisf <duboisf@student.ethz.ch>
2017-01-07 20:47:00 +0100

    cosmetics

--- src/calibration/main.m
+++ src/calibration/main.m
@@ -3,15 +3,15 @@ close all;
 clc;
 
 % set desired options
-options.extract_every_x = 1; % for all images set: 1
+options.extract_every_x = 3; % for all images set: 1
 options.convert_to_grayscale = true;
-options.rotate_images = -1; % rotates images k*90°, 1: Counterclock wise, 0: No Rotation, -1: Clockwise
+options.rotate_images = 1; % rotates images k*90°, 1: Counterclock wise, 0: No Rotation, -1: Clockwise
 options.save_images = true;
 
 %% 
-[~,path_name] = uigetfile('*.*','Select the first image...');
-convertImages2CalibImages(path_name,options);
+[~, path_name] = uigetfile('*.*','Select the first image...');
+convertImages2CalibImages(path_name, options);
 
 %%
-[video_name,path_name] = uigetfile('*.*','Select the video source file...');
-extractImagesFromVideo([path_name,video_name],options);
+[video_name, path_name] = uigetfile('*.*','Select the video source file...');
+extractImagesFromVideo([path_name, video_name], options);
