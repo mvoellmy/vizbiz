283424083ee12d525f82aa842c57864a92450926
Miro Voellmy <mvoellmy@users.noreply.github.com>
2017-01-08 18:52:46 +0100

    Update plotTrajectory_2D.m

--- src/visualization/plotTrajectory_2D.m
+++ src/visualization/plotTrajectory_2D.m
@@ -10,19 +10,19 @@ function plotTrajectory_2D (W_Pos_C, bootstrap_frame_idx_1, bootstrap_frame_idx_
 
 figure('name','Comparison against ground truth');
 hold on;
-plot(W_Pos_C(1,:),W_Pos_C(2,:),'b.-');
+plot(W_Pos_C(1,:),W_Pos_C(2,:),'b.-','LineWidth',8);
 plot(W_Pos_C(1,1),W_Pos_C(2,1),'bsquare');
 plot(W_Pos_C(1,end),W_Pos_C(2,end),'b*');
 
 axis equal;
 
 % Print Frame numbers
-frame_id_txt = ['   ', num2str(bootstrap_frame_idx_1)];
-text(W_Pos_C(1,1), W_Pos_C(2,1), frame_id_txt, 'Color', 'green');
-for i=2:size(W_Pos_C, 3)
-    frame_id_txt = ['   ', num2str(i + bootstrap_frame_idx_2 - 2)];
-    text(W_Pos_C(1,i), W_Pos_C(2,i), frame_id_txt, 'Color', 'green');
-end
+%frame_id_txt = ['   ', num2str(bootstrap_frame_idx_1)];
+%text(W_Pos_C(1,1), W_Pos_C(2,1), frame_id_txt, 'Color', 'green');
+%for i=2:size(W_Pos_C, 3)
+%    frame_id_txt = ['   ', num2str(i + bootstrap_frame_idx_2 - 2)];
+%    text(W_Pos_C(1,i), W_Pos_C(2,i), frame_id_txt, 'Color', 'green');
+%end
 
 xlabel('x');
 ylabel('y');
