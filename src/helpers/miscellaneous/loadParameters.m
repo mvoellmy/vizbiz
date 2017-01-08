038e53e9eacf7864c554b4c3e7aa3b516d59b1cb
Pascal Buholzer <buhpasca@student.ethz.ch>
2017-01-08 03:11:54 +0100

    better a bit

--- src/helpers/miscellaneous/loadParameters.m
+++ src/helpers/miscellaneous/loadParameters.m
@@ -112,16 +112,16 @@ if params.ds == 0
     
     params.cont.landmarks_cutoff = 250;  % --------------------------------------
     
-    params.localization_ransac.num_iterations_pnp = 1000; % 2000 fix?
+    params.localization_ransac.num_iterations_pnp = 1200; % 2000 fix?
     params.localization_ransac.pixel_tolerance = 2; % 10 [pixels]  % -------------------------------------
     
     params.kp_tracker.min_nr_landmarks = 600;
     params.kp_tracker.min_nr_landmarks_bearing_angle_adapt = 150;
     params.kp_tracker.bearing_angle_multiplicator = 1.75;
-    params.kp_tracker.max_nr_candidates = 1200;  % --------------------------------------
+    params.kp_tracker.max_nr_candidates = 1300;  % --------------------------------------
     params.kp_tracker.rand_pick = false;
     params.kp_tracker.nr_best_candidates = 100; % for randomized picking
-    params.kp_tracker.bearing_low_thr = 1.6; % [deg]  % --------------------------------------
+    params.kp_tracker.bearing_low_thr = 1.65; % [deg]  % --------------------------------------
     params.kp_tracker.bearing_up_thr = params.kp_tracker.bearing_low_thr*10.5; % [deg]
     params.kp_tracker.min_nr_trackings = 2; % 3
     params.kp_tracker.max_nr_trackings = 25;
